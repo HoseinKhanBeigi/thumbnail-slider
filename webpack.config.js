89d36cc5bbd6fd1da418c5304fed8051481f59c9
hosein <hoseinkhanbeigi@gmail.com>
2019-02-13 05:20:04 +0330

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+    library: "ThumbnailSlider",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
